5a7dc44349cb4e58018e639f0445cd23f27bf0e4
melike <melkedemr@gmail.com>
2022-07-12 20:36:06 +0100

    Initial commit

---
+++ src/test/java/com/melnac/Day1/SimpleGetRequest.java
@@ -0,0 +1,22 @@
+package com.melnac.Day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+    String url = "http://34.230.72.55:8000/api/spartans";
+
+    @Test
+    public void test1() {
+        //send a get request and save response inside the response object
+        Response response = RestAssured.get(url);
+        //print response status code
+        System.out.println(response.statusCode());
+        //print body
+        response.prettyPrint();
+
+    }
+
+
+}
